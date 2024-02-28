19fffe4ca0ba2221917fd3c88e41f55f72902244
Aronmero <aronmedinasp@gmail.com>
2024-02-28 12:19:36 +0000

    vistas basicas

---
+++ src/components/Auth/AuthUsuario.vue
@@ -0,0 +1,97 @@
+<script setup>
+import { ref } from "vue";
+const emit = defineEmits(["submit"]);
+defineProps({
+  titulo: String,
+  descripcion: String,
+  grupos: Array,
+  profesores: Array,
+  error: String,
+});
+
+const email = ref("");
+const password = ref("");
+
+function submitFormulario() {
+  if (email.value && password.value) {
+    emit("submit", { email: email.value, password: password.value });
+  } else {
+    console.warn("Invalido");
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2 class="text-white text-center">{{ titulo }}</h2>
+    <form method="post" @submit.prevent="submitFormulario">
+      <div>
+        <label class="text-white">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="email@email.com"
+          v-model="email"
+        />
+      </div>
+      <div>
+        <label class="text-white">Contraseña</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Tu contraseña"
+          v-model="password"
+        />
+      </div>
+      <div><input type="submit" /></div>
+    </form>
+    <div v-if="error" class="error-message">{{ error }}</div>
+  </div>
+</template>
+<style scoped>
+.error-message {
+  color: red;
+  margin-top: 10px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  label {
+    margin-left: 5px;
+    margin-bottom: 5px;
+    font-weight: bold;
+    align-self: flex-start;
+  }
+
+  > div {
+    margin: 5px;
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  input {
+    box-sizing: border-box;
+    padding: 15px 12px;
+    width: 320px;
+    border-radius: 12px;
+    font-size: 15px;
+    border: 1px solid #CFBF9E;
+  }
+
+  div:has(input[type="submit"]) {
+    margin-top: 20px;
+
+    input {
+      font-weight: 600;
+      border-radius: 20px;
+      background-color: #CEA647;
+      cursor: pointer;
+      border-color: #CEA647;
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,19 @@ import { createRouter, createWebHistory } from "vue-router";
  * Definición de las rutas del enrutador.
  */
 const routes = [
-  { path: "/", component: () => import("@/views/home.vue") }
+  {
+    path: "/",
+    component: () => import("@/views/login.vue"),
+  },
+  {
+    path: "/salas",
+    component: () => import("@/views/salas.vue"),
+  },
+  { path: "/eventos", component: () => import("@/views/eventos.vue") },
+  {
+    path: "/reservas",
+    component: () => import("@/views/reservas.vue"),
+  },
 ];
 
 /**
