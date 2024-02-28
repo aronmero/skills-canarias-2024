89c94f7efa0701902f073c28902bca8457b659a6
Aronmero <aronmedinasp@gmail.com>
2024-02-28 13:16:30 +0000

    Controllador usuario

--- app/Http/Controllers/apiUsersController.php
+++ app/Http/Controllers/apiUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\userRequest;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class apiUsersController extends Controller
@@ -10,9 +13,14 @@ class apiUsersController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(userRequest $request)
     {
-        
+        $user=User::create([
+            'nombre' => $request->input('nombre'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'),)
+        ])->assignRole('Usuario');
+        return  parent::respuestaHTTP("Usuario creado satisfactoriamente.ID: ". $user->id, 201, true);
     }
 
     /**
@@ -20,7 +28,12 @@ class apiUsersController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $usuario = User::findOrFail($id);
+            $usuario->makeVisible(['id']);
+            return $this->respuestaHTTP($usuario, 200, true);
+        } catch (ModelNotFoundException $exception) {
+            return $this->respuestaHTTP('Usuario no encontrado', 404, false);
+        }
     }
-
 }
